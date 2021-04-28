6d78c48cfc24c7752bb579c42f878f5c44095530
Vu Duc <vuduc1711@gmail.com>
2021-04-28 14:23:43 +0700

    [feature] - update api

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
-import bottle
+import bottle,re,json
 from bottle import get,post,delete,put,request,response
-import re,json
-users = [{'name':'vuduc','password':'123123123','email':'vuduc1711@gmail.com'}]
+
+import models.database as db
+
 app = application = bottle.default_app()
-name_pattern = re.compile(r'^[a-zA-Z\d]{1,64}$')
-password_pattern = re.compile(r'[A-Za-z0-9@#$%^&+=]{8,}')
+name_pattern = re.compile(r'^[a-zA-Z\d]{1,29}$')
+password_pattern = re.compile(r'[A-Za-z0-9@#$%^&+=]{8,16}')
 email_pattern = re.compile(r'^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$')
 
-# GET
+
 @get('/all')
 def getAll():
+    """ GET ALL """
     response.headers['Content-Type'] = 'application/json'
-    return json.dumps(users)
+    response.status = 200
+    users = db.create_query_get("1' or 1 = 1 --")
+    return {
+        'data': json.dumps(users),
+        'status': response.status
+    }
+
+
 @get('/get-one')
 def getOne():
+    """ GET ONE """
     try:
         if request.query.name is None:
             raise ValueError
-        if  request.query.name not in [user['name'] for user in users] :
-            raise KeyError
         name = request.query.name
+        users = db.create_query_get(name)
+        if users == []:
+            raise KeyError
         response.headers['Content-Type'] = 'application/json'
-        return json.dumps([user for user in users if user['name'] == name])
+        response.status = 200
+        return {
+            'data': json.dumps(users),
+            'status': response.status
+        }
     except ValueError:
         response.status = 400
-        return
+        return {'errors': response.status}
     except KeyError:
         response.status = 409
-        return
+        return {'errors': response.status}
 
 
-#POST
 @post('/add-user')
 def addUser():
+    """ POST """
     try:
         # parse input data
         try:
@@ -49,6 +64,7 @@ def addUser():
             name = data['name']
             email = data['email']
             password = data['password']
+            users = db.create_query_get("1' or 1 = 1 --")
         except (TypeError, KeyError):
             raise ValueError
 
@@ -59,24 +75,29 @@ def addUser():
     except ValueError:
         # if bad request data, return 400 Bad Request
         response.status = 400
-        return
+        return {'errors': response.status}
 
     except KeyError:
         # if name already exists, return 409 Conflict
         response.status = 409
-        return
+        return {'errors': response.status}
 
     # add user
-    users.append({'name': name ,'password' : password, 'email': email})
+    db.create_query_insert(name,password,email)
 
     # return 200 Success
     response.headers['Content-Type'] = 'application/json'
-    return json.dumps(users)
+    return {
+        'data': json.dumps(db.create_query_get("1' or 1 = 1 --")),
+        'status': response.status
+    }
 
-#PUT
+
 @put('/update-user')
 def updateUser():
+    """ PUT """
     try:
+        users = db.create_query_get("1' or 1 = 1 --")
         if request.query.name is None:
             raise ValueError
         if request.query.name not in [user['name'] for user in users]:
@@ -94,19 +115,42 @@ def updateUser():
             newEmail = data['email']
         if data['password'] is not None or password_pattern.fullmatch(data['password'] is not None):
             newPass = data['password']
-        for user in users:
-            if user['name'] == name:
-                if newEmail is not None:
-                    user['email'] =newEmail
-                if newPass is not None:
-                    user['password'] = newPass
+        user = db.create_query_get(name)
+        if newEmail is None:
+            newEmail = user['email']
+        if newPass is None:
+            newPass = user['newPass']
+        db.create_query_update(name,newPass,newEmail)
     except ValueError:
         response.status = 400
-        return
+        return {'errors': response.status}
     except KeyError:
         response.status = 409
-        return
+        return {'errors': response.status}
     response.headers['Content-Type'] = 'application/json'
-    return json.dumps(users)
+    return {
+        'data': json.dumps(db.create_query_get('1 or 1 = 1 --')),
+        'status': response.status
+    }
+
+
+@delete('/delete-user')
+def deleteUser():
+    """ DELETE """
+    try:
+        users = db.create_query_get("1' or 1 = 1 --")
+        if request.query.name is None:
+            raise KeyError
+        db.create_query_delete(request.query.name)
+        response.status = 200
+        return {
+            'data' : json.dumps(db.create_query_get("1' or 1 = 1 --")),
+            'status' : response.status
+        }
+    except KeyError:
+        response.status = 409
+        return {'errors': response.status}
+
+
 if __name__ == '__main__':
     bottle.run(host = '127.0.0.1', port = 8000)
